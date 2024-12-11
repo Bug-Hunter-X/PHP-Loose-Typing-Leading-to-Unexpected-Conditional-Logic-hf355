5ea34c55c61dfec4e7c1a7a0bd4bd972d6fb0fb2
BugHunter-X <askzenai@gmail.com>
2024-12-11 07:58:05 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,12 @@
+function myFunction() {
+  // Some code here
+  return $value;
+}
+
+// ... later in the code ...
+
+if (myFunction() === null) {
+  // Handle null value
+} else if (myFunction() === false) {
+  // Handle false value
+}
